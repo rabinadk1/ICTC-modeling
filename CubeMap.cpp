ce9b2eb8c330a6ad91828a47c5c22e98b9b591d3
rabinadk1 <rabin.adk1@gmail.com>
2020-03-03 21:10:13 +0545

    Fixed bug in texture loading by providin glTexParamater for GL_TEXTURE_CUBE_MAP instead of GL_TEXTURE_2D

--- CubeMap.cpp
+++ CubeMap.cpp
@@ -10,6 +10,7 @@ CubeMap::CubeMap(const std::vector<std::string> &faces)
 
   int width, height, nrChannels;
   u_char *localBuffer;
+  stbi_set_flip_vertically_on_load(false);
   for (uint i = 0; i < faces.size(); ++i)
   {
     /*
@@ -27,11 +28,11 @@ CubeMap::CubeMap(const std::vector<std::string> &faces)
   }
 
   // set the texture wrapping/filtering options (on the currently bound texture object)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
   // Unbinding texture
   Unbind();
